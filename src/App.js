e6a87bbe9a18e7bc622b6bde50f44a2342678ffd
MERCY NJAMBI <njambim695@gmail.com>
2025-03-19 23:13:36 +0300

    Update project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero"
 import About from "./components/About/About";
